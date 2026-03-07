4ba88e67f413eaab0784bda49332bdff42ecc36c
dharshan <dharsofficialacc@gmail.com>
2026-03-07 13:29:04 +0530

    fix: remove conversion_factor to resolve AttributeError

--- production_entry/production_entry/doctype/shaft_production_run/shaft_production_run.py
+++ production_entry/production_entry/doctype/shaft_production_run/shaft_production_run.py
@@ -184,7 +184,6 @@ class ShaftProductionRun(Document):
                         "qty": suggested_qty,
                         "uom": wo_item.stock_uom,
                         "stock_uom": wo_item.stock_uom,
-                        "conversion_factor": wo_item.conversion_factor or 1.0,
                         "is_finished_item": 0,
                     })
 
