32e52ffedcdbbccdc4e50cdc930ad95bc3beeafb
dharshan <dharsofficialacc@gmail.com>
2026-04-03 12:45:29 +0530

    Revert "fix(planning sheet): unblock Desk route vs Workspace, System Manager perms, PE Planning Hub workspace"
    This reverts commit 6bc31dd26a17d91769fe18abd9b271c1bccc07ea.

--- production_entry/hooks.py
+++ production_entry/hooks.py
@@ -63,8 +63,6 @@ doctype_js = {
 # before_install = "production_entry.install.before_install"
 # after_install = "production_entry.install.after_install"
 
-after_migrate = ["production_entry.setup.after_migrate"]
-
 # Desk Notifications
 # -------------------
 # See frappe.core.notifications.get_notification_config
