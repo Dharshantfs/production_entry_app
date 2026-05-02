f136a50e699df96c677e74d3739414aff610b992
dharshan <dharsofficialacc@gmail.com>
2026-05-02 12:30:08 +0530

    Optimize app structure by removing legacy copies

--- production_scheduler_patch/production_scheduler/production_scheduler/doctype/production_merge/production_merge.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Production Scheduler
-// License: MIT
-
-frappe.ui.form.on('Production Merge', {
-    refresh(frm) {
-        // Custom UI logic can be added here if needed
-    }
-});
--- ps_full/production_scheduler/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Production Scheduler App - Smart Production Scheduling and Capacity Management"""
-__version__ = '0.0.2'
